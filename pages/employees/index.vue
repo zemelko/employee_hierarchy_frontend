f4f9da25920dceef7e3902e0ac534d67798e7c2a
NikolayZemelko <zemelko1989@gmail.com>
2024-01-19 12:55:41 +0300

    Add Employees Table
    1) Add Sorting, Filtering and Pagination

--- pages/employees/index.vue
+++ pages/employees/index.vue
@@ -1,12 +1,87 @@
 <script setup>
-const { data: employees } = await useFetch('http://127.0.0.1:8000/api/v1/employees/')
+
+const columns = [
+    {
+        key: 'id',
+        label: 'ID',
+        sortable: true
+    },
+    {
+        key: 'full_name',
+        label: 'Name',
+        sortable: true
+    },
+    {
+        key: 'photo',
+        label: 'Photo'
+    },
+    {
+        key: 'date_offered',
+        label: 'Oferred',
+        sortable: true
+    },
+    {
+        key: 'job_title',
+        label: 'Job position',
+        sortable: true
+    },
+    {
+        key: 'supervisor.full_name',
+        label: 'Supervisor',
+        sortable: true
+    },
+    {
+        key: 'salary',
+        label: 'Salary',
+        sortable: true
+    }
+]
+
+// Fetching Data
+const { data: employees } = await useFetch(`http://127.0.0.1:8000/api/v1/employees`)
+const employeesArray = toRaw(employees.value)
+
+// Selecting columns
+const selectedColumns = ref([...columns])
+
+// Filtering 
+const q = ref('')
+
+const filteredRows = computed(() => {
+  if (!q.value) {
+    return employeesArray
+  }
+
+  return employeesArray.filter((employee) => {
+    return Object.values(employee).some((value) => {
+      return String(value).toLowerCase().includes(q.value.toLowerCase())
+    })
+  })
+})
+
+// Paginating
+const page = ref(1)
+const pageCount = 10
+
+const filteredAndPaginatedRows = computed(() => {
+  return filteredRows.value.slice((page.value - 1) * pageCount, (page.value) * pageCount)
+})
+
 </script>
 
 <template>
     <p>Employees List page</p>
-    <Employee
-    v-for="employee in employees.results" 
-    v-bind:key="employee.id"
-    v-bind:employee="employee"
-    />
+    <div>
+        <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+            <USelectMenu v-model="selectedColumns" :options="columns" multiple placeholder="Columns" />
+        </div>
+        <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+            <UInput v-model="q" placeholder="Filter employee..." />
+        </div>
+
+        <UTable :rows="filteredAndPaginatedRows" :columns="selectedColumns" />
+        <div class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
+            <UPagination v-model="page" :page-count="pageCount" :total="filteredRows.length" />
+        </div>
+    </div>
 </template>
